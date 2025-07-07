4758740bcbbd3dffc73e8eefdd32b8ef3b1296d7
Pankaj Joshi <127141403+Pankajjoshi11@users.noreply.github.com>
2025-07-07 12:57:22 +0530

    changed config for the deployement

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,10 +1,10 @@
 const config = {
   development: {
-    API_BASE_URL: 'http://localhost:5000'
+    API_BASE_URL: 'http://localhost:5000' || 'https://linkedin-optimizer.onrender.com'
   },
   production: {
     // In production, frontend and backend are served from the same domain
-    API_BASE_URL: ''
+    API_BASE_URL: 'https://linkedin-optimizer.onrender.com'
   }
 };
 
